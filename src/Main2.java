76b0e0ba5a6d0aab17330b01e8e16dea1bb58dd5
ASUS <successolga118@gmail.com>
2025-06-07 09:54:09 +0300

    task 2

---
+++ src/Main2.java
@@ -0,0 +1,35 @@
+
+// task 2
+//Допустим, дан текстовый файл. Ваша задача,
+//вывести содержимое этого файла на экран.
+//
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main2 {
+    public static void main(String[] args) {
+        String target = "output.txt";
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            BufferedReader fis = new BufferedReader(new FileReader(target));
+            int data;
+            while ((data = fis.read()) != -1) {
+                sb.append((char) data);}
+            System.out.println(sb);
+            }
+        catch(FileNotFoundException e){
+                System.out.println("File not found");
+
+            }
+            catch(IOException e ){
+                System.out.println("Mistake");
+            }
+
+            }
+
+        }
+
